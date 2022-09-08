7e8afdb5d039f7d46359184e7789aa9954ccc384
huypham0427 <ptanhhuy27@yahoo.com>
2022-09-08 08:15:52 -0700

    Initial commit

---
+++ src/main/java/com/example/SwaggerwithSpringBoot/SwaggerwithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwaggerwithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerwithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerwithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SwaggerwithSpringBoot/SwaggerwithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SwaggerwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerwithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
